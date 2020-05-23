854be990be36ce18a2f95e0d13cf1367d779f037
pawelek9 <pawelklimczyk.9@gmail.com>
2020-05-23 16:42:13 +0200

    skrypt do pobierania danych zapisuje jako liste

--- downloader.R
+++ downloader.R
@@ -1,14 +1,13 @@
-print('hello')
-read.csv("https://stooq.pl/q/d/l/?s=pko&d1=20180101&d2=&i=d")
-
+## this file is responsible to download data
 download_frame <- function(company,first_date= 20180101, last_date= 20190101) {
   #This function download one frame
   #args:
   #company = name of comapany that values of shares will be downloaded
   #first_date = start date of downloaded time series
   #last_date = end date of downloaded time series
+  
   link = trimws(paste("https://stooq.pl/q/d/l/?s=",company,'&d1=',first_date, '&d2=',
-               last_date,'&i=d'))
+                      last_date,'&i=d'))
   data = read.csv(link)
   
   return(data)
@@ -17,15 +16,11 @@ download_frame <- function(company,first_date= 20180101, last_date= 20190101) {
 download_data <- function(vect) {
   #Extension to download_frame fuction, instead of one variable it takes vector of
   #variables
+  
   var_list = list()
   for (i in vect) {
     var_list[[i]] = download_frame(i, 20180101, 20190101)
   }
   
   return(var_list)
-  
 }
-
-x = c('pko', 'ing', 'kgh')
-
-data = download_data(x)
