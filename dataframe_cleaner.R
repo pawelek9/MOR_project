d0436ce0251ded9992572b1ea4edfa60416e7526
pawelek9 <pawelklimczyk.9@gmail.com>
2020-05-23 18:57:51 +0200

    adding comments

--- dataframe_cleaner.R
+++ dataframe_cleaner.R
@@ -1,6 +1,8 @@
 ##This script will be responsible for data preparing
 
 count_ln_return_rate_one_frame <- function(frame) {
+  #this function computes log return rates for one frame
+  
   frame = cbind(frame, (shift(frame, -1)))[-1,]
   frame = frame[,c(1,2,4)]
   colnames(frame) = c('Data','Zamkniecie','Poprzednie_Zamkniecie')
@@ -10,6 +12,8 @@ count_ln_return_rate_one_frame <- function(frame) {
 }
 
 count_ln_returns <- function(values) {
+  #this function computes log return for all frames
+  
   R_lns= lapply(values, count_ln_return_rate_one_frame)
   
   return(R_lns)
