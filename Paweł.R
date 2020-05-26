bf4b416d219f8c62883c4fc42345a3f3d875b2fc
pawelek9 <pawelklimczyk.9@gmail.com>
2020-05-26 18:35:45 +0200

    commit

--- Paweł.R
+++ Paweł.R
@@ -7,8 +7,16 @@ source('Portfolio.R')
 private_companies = download_data(companies_privat, 20180101, 20200101)
 public_companies = download_data(companies_public, 20180101, 20200101)
 
-private_companies_r_lns =  count_ln_returns(private_companies, companies_public) 
-public_companies_r_lns = count_ln_returns(public_companies, companies_privat)
+public_companies_r_lns =  count_ln_returns(public_companies, companies_public) 
+private_companies_r_lns = count_ln_returns(private_companies, companies_privat)
 
-optm = Optimal_portfolio(private_companies_r_lns, 'LongOnly')
+optm_priv = Optimal_portfolio(private_companies_r_lns, 'LongOnly')
+optm_public = Optimal_portfolio(public_companies_r_lns, 'LongOnly')
+
+weighted_rates_public = rates_using_portfolio(public_companies_r_lns, optm_public)
+weighted_rates_private = rates_using_portfolio(private_companies_r_lns, optm_priv) 
+
+return_priv = count_return(weighted_rates_private)
+return_public = count_return(weighted_rates_public)
+
 
--- portfolio.R
+++ portfolio.R
@@ -21,3 +21,34 @@ Optimal_portfolio <- function(dataframe, constraints) {
                     'maxratio_portfolio', 'minrisk_portfolio')
   return(output)
 }
+
+
+rates_using_portfolio <- function(data, porfolio) {
+  weights = porfolio$efficeint_portfolio@portfolio@portfolio$weights
+  data = data %>% as.data.frame()
+  print(data %>% head())
+  dfs = list()
+  for (i in names(weights)) {
+    dfs[i] = weights[i]* data[i]
+  }
+  
+  dfs = dfs %>% as.data.frame()
+  rownames(dfs) = data %>% row.names()
+  return(dfs %>% as.data.frame())
+}
+
+
+count_return  <- function(weighted_returns) {
+  retr_rates = exp(apply(weighted_returns, 1, sum))
+  retr<- c()
+  w_p1 <- 10000
+  
+  for(i in 1:length(retr_rates)){
+    w_p1[i+1] <- retr_rates[i]*w_p1[i]
+  }
+  
+  return(w_p1)
+}
+
+
+
